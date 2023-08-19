53c2688c842914d6807e1b0c282ce52cf4cac3b7
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-19 19:48:06 +0545

    test button, icon, and toast

--- app/src/main/java/com/app/testcompose/MainActivity.kt
+++ app/src/main/java/com/app/testcompose/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.app.testcompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.app.testcompose.ui.theme.TestComposeTheme
 
@@ -29,8 +31,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    Column() {
+    Column {
+        val context = LocalContext.current
+
         Text(text = "Hello")
         Text(text = "World")
+        Button(onClick = {
+            Toast.makeText(context, "Info button pressed", Toast.LENGTH_SHORT).show()
+        }) {
+            Icon(Icons.Filled.Info, contentDescription = "Info")
+        }
     }
 }
