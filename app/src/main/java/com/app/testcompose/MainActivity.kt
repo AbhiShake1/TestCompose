c187d0fabc53cfe2ef99c08d05f44fe747bd3665
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-08-19 20:22:16 +0545

    recreate flutters counter

--- app/src/main/java/com/app/testcompose/MainActivity.kt
+++ app/src/main/java/com/app/testcompose/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.app.testcompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -23,23 +25,33 @@ class MainActivity : ComponentActivity() {
             TestComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting()
+                    Home()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting() {
-    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        val context = LocalContext.current
-        Text(text = "Hello")
-        Text(text = "World")
-        ExtendedFloatingActionButton(
-            onClick = { Toast.makeText(context, "Info button pressed", Toast.LENGTH_SHORT).show() },
-            icon = { Icon(Icons.Filled.Info, contentDescription = "Info") },
-            text = { Text(text = "Info") }
-        )
+fun Home() {
+    val counterState = remember {
+        mutableStateOf(0)
     }
+    Scaffold(
+        topBar = { CenterAlignedTopAppBar(title = { Text(text = "Test Compose App") }) },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {counterState.value++}) {
+                Icon(Icons.Outlined.Add, contentDescription = "Add icon")
+            }
+        },
+        content = {
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(it), contentAlignment = Alignment.Center) {
+                Text(text = "You pressed the button ${counterState.value} times")
+            }
+        }
+    )
 }
